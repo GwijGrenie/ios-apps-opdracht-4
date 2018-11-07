87224195367a7beed882d365debc0f050bef9f51
Gwij Grenié <gwij.grenie@student.vives.be>
2018-11-07 10:46:03 +0100

    Add kitties

--- Opdracht_4_Digitale_Klok/ViewController.swift
+++ Opdracht_4_Digitale_Klok/ViewController.swift
@@ -1,9 +1,8 @@
 import UIKit
 
-class ViewController: UIViewController, ClockHelperDelegate {
+class ViewController: UIViewController {
     
-    var currentLabelCollections : [[UILabel]] = []
-    var labelMapping : [String : [UILabel]] = [:]
+    @IBOutlet weak var imageClock: UIImageView!
     
     @IBOutlet var labelCollectionHet: [UILabel]!
     @IBOutlet var labelCollectionIs: [UILabel]!
@@ -28,28 +27,38 @@ class ViewController: UIViewController, ClockHelperDelegate {
     @IBOutlet var labelCollectionElf: [UILabel]!
     @IBOutlet var labelCollectionTwaalf: [UILabel]!
     
+    var currentLabelCollections : [[UILabel]] = []
+    var labelMapping : [String : [UILabel]] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initMapping()
+        imageClock.isHidden = true
         ClockHelper(Delegate: self)
     }
     
-    func updateClock(_ results: [String]) {
-        for labelCollection in currentLabelCollections {
-            dimLabels(LabelCollection: labelCollection)
-        }
-        
-        currentLabelCollections = []
-        
-        for result in results {
-            let labelCollection = labelMapping[result]!
-            currentLabelCollections.append(labelCollection)
-            lightLabels(LabelCollection: labelCollection)
-        }
-    }
-    
-    func longPressed(sender: UILongPressGestureRecognizer) {
-        
+    @IBAction func onLongPressGesture(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm"
+        let alert = UIAlertController(title: "Configuratie", message: "Instellen van een wekker", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Annuleer", style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Geef je tijd in als uu:mm bv. 08:30"
+        })
+        alert.addAction(UIAlertAction(title: "Bewaar", style: UIAlertAction.Style.default, handler: { action in
+            let textField = alert.textFields![0]
+            if textField.text == nil || textField.text!.isEmpty {
+                self.imageClock.isHidden = true
+            } else if dateFormatter.date(from: textField.text!) != nil {
+                self.imageClock.isHidden = false
+                self.imageClock.backgroundColor = UIColor.clear
+            } else {
+                self.imageClock.isHidden = false
+                self.imageClock.backgroundColor = UIColor.red
+            }
+            
+        }))
+        self.present(alert, animated: true)
     }
     
     private func initMapping() {
@@ -91,3 +100,18 @@ class ViewController: UIViewController, ClockHelperDelegate {
     }
 }
 
+extension ViewController: ClockHelperDelegate {
+    func updateClock(_ results: [String]) {
+        for labelCollection in currentLabelCollections {
+            dimLabels(LabelCollection: labelCollection)
+        }
+        
+        currentLabelCollections = []
+        
+        for result in results {
+            let labelCollection = labelMapping[result]!
+            currentLabelCollections.append(labelCollection)
+            lightLabels(LabelCollection: labelCollection)
+        }
+    }
+}
